5cf76ca85810a93f04828065a0affd9e23272e48
theOluwafemi <oluwafemidamilola21@gmail.com>
2019-06-23 07:06:55 +0100

    cleaning code

--- controllers/home.js
+++ controllers/home.js
@@ -3,7 +3,6 @@ const Post = require('../database/models/post')
 module.exports = async (req, res) => {
 
    const posts = await Post.find({}).populate('author')
-   console.log(posts);
    res.render('index', {posts});
    
 }
